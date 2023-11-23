c0d57ac39a011375bba52bccb6516f80e06af68a
Artur Kubala <ak@arturkubala.com>
2023-11-23 15:53:12 +0000

    Initial Commit

---
+++ project_countdown/project_countdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_countdownApp.swift
+//  project_countdown
+//
+//  Created by Artur on 23/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct project_countdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
